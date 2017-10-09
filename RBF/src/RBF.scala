19a561dc620cfd8132fc9d3cb83b3b744c6ec73d
Haseeb <11besemjaved@seecs.edu.pk>
2017-10-09 18:31:31 -0400

    Working implementation with just Xs as inputs output does not seem to be as expected

--- RBF/src/RBF.scala
+++ RBF/src/RBF.scala
@@ -1,3 +1,4 @@
+import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 import scala.util.Random
 
@@ -5,37 +6,58 @@ object RBF {
   val seed = 8
   val rand =  new java.util.Random(seed)
   val sampleSize = 75
-  val k = sampleSize/2
+  val totalEpochs = 10000
+  val k = 10//sampleSize/2
+  var inputs = Array[Double](sampleSize)
+  var desiredOutputs = Array[Double](sampleSize)
+  var dataPoints: Map[Double, Double] = null
+
+  var gaussians =  new Array[(Double) => Double](k + 1)
+
   def main(args: Array[String]): Unit = {
 
 //    generateData()
     kMeans()
+    leastMeanSquare(0.02)
   }
 
-  def euclideanNormSquared(i : (Double, Double), j: (Double, Double)): Double ={
+  def euclideanNormSquared(i : Double, j: Double): Double ={
     Math.pow(euclideanNorm(i, j), 2)
   }
 
-  def euclideanNorm(i : (Double, Double), j: (Double, Double)): Double ={
-    Math.sqrt(Math.pow(i._1 - j._1, 2) + Math.pow(i._2 - j._2, 2))
+  def euclideanNorm(i : Double, j: Double): Double ={
+    Math.abs(i - j)
   }
 
-  def generateData(): List[(Double,Double)] ={
+  def generateData() ={
 
-    val dataPoints = (1 to sampleSize).map(i => {
+    dataPoints = (1 to sampleSize).map(i => {
       val x = twoDP(rand.nextDouble)
       val noise = -0.1 + (0.1 - (-0.1)) * rand.nextDouble
       val h = 0.5 + 0.4 * Math.sin(2 * Math.PI * x) + noise
-      (x, twoDP(h))
-    }).toList
+      (x , twoDP(h))
+    }).toMap
+
+//    dataPoints
+
+    inputs = dataPoints.map(points => {
+      points._1
+    }).toArray
+
+    desiredOutputs = dataPoints.map(points =>{
+      points._2
+    }).toArray
+
+//    dataPoints.foreach(println)
+//    println("######################")
+//    inputs.zip(desiredOutputs).foreach({case (x, y) => println(s"($x, $y)")})
 
 //    dataPoints(0)
-
 //    return dataPoints
-    println("DataPoints")
-    dataPoints.foreach(println)
+//    println("DataPoints")
+//    dataPoints.foreach(println)
 //    println(dataPoints.length)
-    dataPoints
+//    dataPoints
   }
 
   def twoDP(num: Double): Double ={
@@ -44,42 +66,41 @@ object RBF {
 
   def kMeans(): Unit ={
     Random.setSeed(seed)
-    val dataPoints = generateData()
-//    var initialMeans = Set[(Double, Double)]()
-//    while (initialMeans.size < k ){
-//      initialMeans = initialMeans + dataPoints(rand.nextInt(dataPoints.size) )
-//    }
+    generateData()
 
-    var initialMeans = Random.shuffle(dataPoints).takeRight(k)
+    var initialMeans = Random.shuffle(inputs.toList).takeRight(k)
     println("Initial Means")
     initialMeans.foreach(println(_))
-    var clusters = collection.mutable.HashMap[(Double, Double), ListBuffer[(Double, Double)]]()//.withDefaultValue(ListBuffer[(Double, Double)]())
-
-
+    var clusters : mutable.Map[Double, ListBuffer[Double]] = null
     var meanChanged = false
     do{
+//      println("Clustering")
+      clusters = mutable.HashMap[Double, ListBuffer[Double]]()//.withDefaultValue(ListBuffer[(Double, Double)]())
       meanChanged = false
-      dataPoints.foreach(dataPoint => {
-        var currentMinimumCluster = (initialMeans(0), Double.PositiveInfinity)
+      inputs.foreach(point => {
+        var currentMinimumCluster = (Double.NegativeInfinity, Double.PositiveInfinity)
         initialMeans.foreach(mean => {
-          val norm = euclideanNormSquared(dataPoint, mean)
+          val norm = euclideanNormSquared(point, mean)
           if(norm < currentMinimumCluster._2 && mean != currentMinimumCluster._1){
             currentMinimumCluster = (mean, norm)
           }
         })
-        var cluster = clusters.getOrElse(currentMinimumCluster._1, ListBuffer[(Double, Double)]())
-        clusters.put(currentMinimumCluster._1, cluster += dataPoint)
+        var cluster = clusters.getOrElse(currentMinimumCluster._1, ListBuffer[Double]())
+        clusters.put(currentMinimumCluster._1, cluster += point)
+//        println(currentMinimumCluster._1, cluster)
       })
 
+//      println("Updating means")
       clusters = clusters.map({case (mean, cluster) => {
-        val clusterSum = cluster.foldLeft((0.0, 0.0))({
-          case ((accA, accB), (a, b)) => (accA + a, accB + b)
+        val clusterSum = cluster.foldLeft((0.0))({
+          case (accA, a) => accA + a
         })
-        val newMean = (twoDP(clusterSum._1 / cluster.size), twoDP(clusterSum._2 / cluster.size))
-//        println(s"NewMean: $newMean - Mean: $mean")
+        val newMean = twoDP(clusterSum / cluster.size)
+//        println(s"OLD: ($mean, $cluster)")
         if(newMean != mean){
           meanChanged = true
         }
+//        println(s"NEW: ($newMean, $cluster)")
         (newMean, cluster)
       }
 
@@ -88,72 +109,76 @@ object RBF {
 
     }while(meanChanged)
 
-  clusters.foreach(println)
+
+    clusters.foreach(println)
+    print("Cluster with just one element: ")
     println(clusters.forall({case (mean, cluster) => cluster.size > 1 }))
 
     val meanVariance = clusters.map({case (mean, cluster) => {
-        val variance = cluster.foldLeft((0.0, 0.0))({
-          case ((accA, accB), (a, b)) => (accA + euclideanNormSquared((a, b), mean), accB + 1)
-        })._1 / cluster.size
+        val variance = cluster.foldLeft((0.0))({
+          case (accA, a) => accA + euclideanNormSquared(a, mean)
+        }) / cluster.size
         (mean, variance)
       }})
 
     meanVariance.foreach(println)
-    val defaulVariance = Math.pow(maxClusterDistance(clusters.keysIterator) / Math.sqrt(2 * k), 2)
-    println(defaulVariance)
-//    clusters.keysIterator.duplicate
+    val defaultVariance = Math.pow(maxClusterDistance(clusters.keysIterator) / Math.sqrt(2 * k), 2)
+    println(defaultVariance)
 
-
-    ////////////////LEAST MEAN SQUARE
-
-    val initialWeights = (1 to k +1 ).map(i => twoDP(rand.nextDouble()))
-
-    val bias = 0.5
-    dataPoints.foreach(point => {
-//      var gaussians = scala.collection.mutable.Map() ++ meanVariance.map({case (mean, variance) => {
-//        gaussian(point, mean, variance)
-//      }})
-      var gaussians = meanVariance.map({case (mean, variance) =>
-        gaussian(point, mean, variance)
+    inputs.foreach(point => {
+      meanVariance.zipWithIndex.foreach({ case ((mean, variance), i) => {
+        gaussians(i) = gaussian(_: Double, mean, variance)
+      }
       })
-//      gaussians = gaussians += 1.0
-
-
-//      gaussians + 1.0
-
-      gaussians.zip(initialWeights)
-
-      val yeild = gaussians.zip(initialWeights).map({case (input ,weight) => input * weight}).sum + bias
-
-      val desired = 1
-      val learningRate = 0.1
-      val x = 8
-      initialWeights.map(weight => weight + (learningRate * (desired - yeild) * x))
-
-//      w(n)+η[d(n)− y(n)]x(n)
-
     })
 
-
-//
+    gaussians(gaussians.length-1) =  gaussian(_: Double, 0, 0)
   }
 
-  def gaussian(input: (Double, Double), center: (Double, Double), variance : Double): Double ={
+  def leastMeanSquare(learningRate : Double ): Unit ={
+    var currentEpoch = 0
+    var weights = new Array[Double](k + 1)
+    //include bias
+    (0 to k ).foreach(i => weights(i) = twoDP(rand.nextDouble()))
+    var yeilds = new Array[Double](inputs.length)
+
+    while(currentEpoch < totalEpochs) {
+      println(s"######## EPOCH $currentEpoch###########")
+      weights.foreach(weight => println(twoDP(weight)))
+      inputs.zipWithIndex.foreach({ case (point, i) => {
+
+        val yeild = gaussians.zip(weights).map({ case (partialGaussian, weight) => partialGaussian(point) * weight}).sum
+        yeilds(i) = yeild
+        weights = weights.map({ case (weight) => {
+          weight + (learningRate * (desiredOutputs(i) - yeild) * point)
+        }
+        })
+      }
+      })
+      currentEpoch+=1
+    }
+
+    println("Desired - Yeild")
+    desiredOutputs.zip(yeilds).foreach({case (desired, yeild) => println(s"$desired - $yeild")})
+  }
+
+  def gaussian(input: Double, center: Double, variance : Double): Double ={
     Math.exp((-1/2*variance) * euclideanNormSquared(input, center))
   }
 
-  def maxClusterDistance (clusters: Iterator[(Double, Double)]): Double ={
+  def maxClusterDistance (clusters: Iterator[Double]): Double ={
     var maxClusterDistance = Double.NegativeInfinity
     var (iter1, iter2) = clusters.duplicate
     iter1.foreach(outer => {
       iter2.foreach(inner => {
         val norm = euclideanNorm(inner, outer)
+        println(s"$norm - $inner - $outer")
         if(norm > maxClusterDistance){
           maxClusterDistance = norm
         }
-
       })
     })
+    println(s"Max Cluster distance: $maxClusterDistance")
     maxClusterDistance
   }
 
